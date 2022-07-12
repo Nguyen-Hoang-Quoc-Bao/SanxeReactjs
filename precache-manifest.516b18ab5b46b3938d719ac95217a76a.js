c1aca17b9eeafa268fd259fb2b910131509c11fe
NGUYEN HOANG QUOC BAO <93quocbaondu1314@gmail.com>
2022-07-12 16:35:54 +0700

    Updates

---
+++ precache-manifest.516b18ab5b46b3938d719ac95217a76a.js
@@ -0,0 +1,422 @@
+self.__precacheManifest = [
+  {
+    "revision": "54d39240d4c435fe6554",
+    "url": "/SanxeReactjs/static/css/main.2b3eecc5.chunk.css"
+  },
+  {
+    "revision": "54d39240d4c435fe6554",
+    "url": "/SanxeReactjs/static/js/main.54d39240.chunk.js"
+  },
+  {
+    "revision": "b78cc117506ffab82960",
+    "url": "/SanxeReactjs/static/js/1.b78cc117.chunk.js"
+  },
+  {
+    "revision": "a3a281cb532354ff244d",
+    "url": "/SanxeReactjs/static/js/runtime~main.a3a281cb.js"
+  },
+  {
+    "revision": "fac4c9d62d79762e07950f361a2be197",
+    "url": "/SanxeReactjs/static/media/Service1.fac4c9d6.png"
+  },
+  {
+    "revision": "99031c1478ddbbb5ed3226aeca077e9e",
+    "url": "/SanxeReactjs/static/media/service2.99031c14.png"
+  },
+  {
+    "revision": "c9d422610eea227aa1e70870c9f0838d",
+    "url": "/SanxeReactjs/static/media/service3.c9d42261.png"
+  },
+  {
+    "revision": "399ac5d1ac0b8706eba1701780484edc",
+    "url": "/SanxeReactjs/static/media/service4.399ac5d1.png"
+  },
+  {
+    "revision": "ee5e96a4e7325945feea38851722a413",
+    "url": "/SanxeReactjs/static/media/img-new-2-1.ee5e96a4.webp"
+  },
+  {
+    "revision": "b16bfc7ed2f6c131f27ab7680c461a9d",
+    "url": "/SanxeReactjs/static/media/img-new-2-2.b16bfc7e.webp"
+  },
+  {
+    "revision": "196b1c2afe78971527e2a3475028e0c1",
+    "url": "/SanxeReactjs/static/media/img-new-2-3.196b1c2a.webp"
+  },
+  {
+    "revision": "a6e81b5a0fe9c142fcdc2e7e56ffcfd5",
+    "url": "/SanxeReactjs/static/media/img-new-2-4.a6e81b5a.webp"
+  },
+  {
+    "revision": "1b07061c6716bdb4a9b0370bc6dd94db",
+    "url": "/SanxeReactjs/static/media/r15-chinh.1b07061c.jpg"
+  },
+  {
+    "revision": "9c02cff5320a16c0e896a9730953d6ae",
+    "url": "/SanxeReactjs/static/media/ab1.9c02cff5.jpg"
+  },
+  {
+    "revision": "2b9ec93e7d87cbaebeb5e40e64216f2e",
+    "url": "/SanxeReactjs/static/media/winnerxx.2b9ec93e.jpg"
+  },
+  {
+    "revision": "ff90ba40c619d8fb485c03f07df1430a",
+    "url": "/SanxeReactjs/static/media/img-new-1.ff90ba40.webp"
+  },
+  {
+    "revision": "f120dcde85db38ba7df5f7390d960ea2",
+    "url": "/SanxeReactjs/static/media/img-new-1-1.f120dcde.webp"
+  },
+  {
+    "revision": "1cdaaf303610ec5754226cea55badde2",
+    "url": "/SanxeReactjs/static/media/img-new-1-2.1cdaaf30.webp"
+  },
+  {
+    "revision": "b50f9e68e2029b30abb8d5eb001e67c5",
+    "url": "/SanxeReactjs/static/media/img-new-1-3.b50f9e68.webp"
+  },
+  {
+    "revision": "161847a11b240869d18b963001d8de5b",
+    "url": "/SanxeReactjs/static/media/img-new-1-4.161847a1.webp"
+  },
+  {
+    "revision": "7ef9d8e1037ad824086fda7988fdc37f",
+    "url": "/SanxeReactjs/static/media/img-new-1-5.7ef9d8e1.webp"
+  },
+  {
+    "revision": "e23a55260ea57efa02c39e0120637085",
+    "url": "/SanxeReactjs/static/media/img-new-1-6.e23a5526.webp"
+  },
+  {
+    "revision": "83555e2f11414da3d8561a5efbf70339",
+    "url": "/SanxeReactjs/static/media/img-new-2.83555e2f.webp"
+  },
+  {
+    "revision": "c194bc16b715673f075ca20361dfd435",
+    "url": "/SanxeReactjs/static/media/img-new-3.c194bc16.webp"
+  },
+  {
+    "revision": "2fb548d682505b89dd973374b2748c7b",
+    "url": "/SanxeReactjs/static/media/Africa twin-4.2fb548d6.jpg"
+  },
+  {
+    "revision": "52b09d3a771f578df65cf709e23f947a",
+    "url": "/SanxeReactjs/static/media/Airblade.52b09d3a.png"
+  },
+  {
+    "revision": "edee36074de0c46299591790d271a6b9",
+    "url": "/SanxeReactjs/static/media/winnerx.edee3607.jpg"
+  },
+  {
+    "revision": "35120984e1109b601fbaa9c87ae2f54b",
+    "url": "/SanxeReactjs/static/media/blade.35120984.png"
+  },
+  {
+    "revision": "a3d1386dc21efc3732f0c18e9cc52a87",
+    "url": "/SanxeReactjs/static/media/exciter.a3d1386d.png"
+  },
+  {
+    "revision": "2c2a8b115572e3024e57788ff8730db8",
+    "url": "/SanxeReactjs/static/media/sirius1.2c2a8b11.png"
+  },
+  {
+    "revision": "3dbc15d2169419fe6fbcc52eacf40fd9",
+    "url": "/SanxeReactjs/static/media/môtbike.3dbc15d2.png"
+  },
+  {
+    "revision": "d1744b7353aeca1a8d4e902285d9e338",
+    "url": "/SanxeReactjs/static/media/Africa twin.d1744b73.jpg"
+  },
+  {
+    "revision": "67fd55680e61c72b4da2cdca35fcdca9",
+    "url": "/SanxeReactjs/static/media/Africa twin-1.67fd5568.jpg"
+  },
+  {
+    "revision": "11d5c675cfffc1e1154b35fe367d119e",
+    "url": "/SanxeReactjs/static/media/Africa twin-2.11d5c675.jpg"
+  },
+  {
+    "revision": "683356bdf0e0e4c767d1f288c7c131f0",
+    "url": "/SanxeReactjs/static/media/Africa twin-3.683356bd.jpg"
+  },
+  {
+    "revision": "3f0be581b52c305ac92dbcc09a99df8d",
+    "url": "/SanxeReactjs/static/media/Africa twin-5.3f0be581.jpg"
+  },
+  {
+    "revision": "6071966c7f460d02350c83ae942d0487",
+    "url": "/SanxeReactjs/static/media/Africa twin-6.6071966c.jpg"
+  },
+  {
+    "revision": "5459c9b2f925b930a96a658a4e2a09a2",
+    "url": "/SanxeReactjs/static/media/Africa twin-7.5459c9b2.jpg"
+  },
+  {
+    "revision": "67fd55680e61c72b4da2cdca35fcdca9",
+    "url": "/SanxeReactjs/static/media/Africa twin-8.67fd5568.jpg"
+  },
+  {
+    "revision": "9a9ffef3741ce0be8e6d7b25030d06e4",
+    "url": "/SanxeReactjs/static/media/ab2.9a9ffef3.jpg"
+  },
+  {
+    "revision": "50ad388d130ae316ca75803f2f31357f",
+    "url": "/SanxeReactjs/static/media/ab3.50ad388d.jpg"
+  },
+  {
+    "revision": "55c7c2763a033d6d30b2025cc6f7636b",
+    "url": "/SanxeReactjs/static/media/ab4.55c7c276.jpg"
+  },
+  {
+    "revision": "de0c9b3d046854d867f2291fd99ec10e",
+    "url": "/SanxeReactjs/static/media/ab5.de0c9b3d.jpg"
+  },
+  {
+    "revision": "5620a011f22c9311cde8076fcd69fc6d",
+    "url": "/SanxeReactjs/static/media/ab6.5620a011.jpg"
+  },
+  {
+    "revision": "9bceefb29bc5e0ef301c154a7040e524",
+    "url": "/SanxeReactjs/static/media/ab7.9bceefb2.jpg"
+  },
+  {
+    "revision": "22cfb08cc8b398f12bdb6713da3039e9",
+    "url": "/SanxeReactjs/static/media/ab8.22cfb08c.jpg"
+  },
+  {
+    "revision": "7f104b530907969290c915c74e3f3326",
+    "url": "/SanxeReactjs/static/media/winnerx-1.7f104b53.jpg"
+  },
+  {
+    "revision": "94aba9086b59080e6eadb8731484c65c",
+    "url": "/SanxeReactjs/static/media/winnerx-2.94aba908.jpg"
+  },
+  {
+    "revision": "ada19a64bfc1bb25ebfd68f270bf5711",
+    "url": "/SanxeReactjs/static/media/winnerx-3.ada19a64.jpg"
+  },
+  {
+    "revision": "af4077f1294d7387bb7cb0004d938d9c",
+    "url": "/SanxeReactjs/static/media/winnerx-4.af4077f1.jpg"
+  },
+  {
+    "revision": "ef0f8b6e71b7956e02afe03f6870d604",
+    "url": "/SanxeReactjs/static/media/winnerx-5.ef0f8b6e.jpg"
+  },
+  {
+    "revision": "e03b123fce71292e92694da677eca2e1",
+    "url": "/SanxeReactjs/static/media/winnerx-6.e03b123f.jpg"
+  },
+  {
+    "revision": "7e6f56eac0fcc9b333960ad0fe12f854",
+    "url": "/SanxeReactjs/static/media/winnerx-7.7e6f56ea.jpg"
+  },
+  {
+    "revision": "2e0d9d7b7f279ba8c1d3db6113084d73",
+    "url": "/SanxeReactjs/static/media/winnerx-8.2e0d9d7b.jpg"
+  },
+  {
+    "revision": "ba9803413a7b0acd5f561dbf408e8fdb",
+    "url": "/SanxeReactjs/static/media/blade-1.ba980341.png"
+  },
+  {
+    "revision": "2e5d9c25cd0ea6597e373ad7aac88990",
+    "url": "/SanxeReactjs/static/media/blade-2.2e5d9c25.png"
+  },
+  {
+    "revision": "fe62182d806aaca0e696415d32a996f6",
+    "url": "/SanxeReactjs/static/media/blade-3.fe62182d.png"
+  },
+  {
+    "revision": "813e4393882f92ca4bd8dd4611580633",
+    "url": "/SanxeReactjs/static/media/blade-4.813e4393.png"
+  },
+  {
+    "revision": "fb4de6a8368bb499b06475c72c1aee48",
+    "url": "/SanxeReactjs/static/media/blade-5.fb4de6a8.png"
+  },
+  {
+    "revision": "b9e71c5fcabb72e2d0fd3ac517a69ced",
+    "url": "/SanxeReactjs/static/media/blade-6.b9e71c5f.png"
+  },
+  {
+    "revision": "1da8e02a5b229b52183474bd5da4d201",
+    "url": "/SanxeReactjs/static/media/blade-7.1da8e02a.png"
+  },
+  {
+    "revision": "bed7577f733f59557748518786f3ad04",
+    "url": "/SanxeReactjs/static/media/blade-8.bed7577f.png"
+  },
+  {
+    "revision": "13608ce77d50b0dc1f55d50912a685f0",
+    "url": "/SanxeReactjs/static/media/exciter-1.13608ce7.png"
+  },
+  {
+    "revision": "0bafdc6cdc699799ca166a98894817e2",
+    "url": "/SanxeReactjs/static/media/exciter-2.0bafdc6c.png"
+  },
+  {
+    "revision": "5ee3ca3d9fd71798d1945df48ec20886",
+    "url": "/SanxeReactjs/static/media/exciter-3.5ee3ca3d.png"
+  },
+  {
+    "revision": "abb819c4b38a0857bac699540efa8ccd",
+    "url": "/SanxeReactjs/static/media/exciter-4.abb819c4.png"
+  },
+  {
+    "revision": "74abdb920c3b6266802971217394b11f",
+    "url": "/SanxeReactjs/static/media/exciter-5.74abdb92.png"
+  },
+  {
+    "revision": "3923e161c614a0df0e7b2427a03dde15",
+    "url": "/SanxeReactjs/static/media/exciter-6.3923e161.png"
+  },
+  {
+    "revision": "67dbd45493c4ba151e2f6db254d4c3fd",
+    "url": "/SanxeReactjs/static/media/exciter-7.67dbd454.png"
+  },
+  {
+    "revision": "6ac5e376bc55c0ed037f19ff9521ccd0",
+    "url": "/SanxeReactjs/static/media/exciter-8.6ac5e376.png"
+  },
+  {
+    "revision": "03a2cf64d332d2ee62c77aaf018e7269",
+    "url": "/SanxeReactjs/static/media/exciter-9.03a2cf64.png"
+  },
+  {
+    "revision": "9156c574f9775ecf11f3a66cc8b08063",
+    "url": "/SanxeReactjs/static/media/sirius-1.9156c574.jpg"
+  },
+  {
+    "revision": "4427bf625c9796cac62661dee99e8213",
+    "url": "/SanxeReactjs/static/media/sirius-2.4427bf62.jpg"
+  },
+  {
+    "revision": "02a5ded0f3821f79ab10d72d23f2daac",
+    "url": "/SanxeReactjs/static/media/sirius-3.02a5ded0.jpg"
+  },
+  {
+    "revision": "0980421e5920fe519ca0533fb7c147a8",
+    "url": "/SanxeReactjs/static/media/sirius-4.0980421e.jpg"
+  },
+  {
+    "revision": "cdce222e4a07a23c97b86bd567339224",
+    "url": "/SanxeReactjs/static/media/sirius-5.cdce222e.jpg"
+  },
+  {
+    "revision": "2c2a8b115572e3024e57788ff8730db8",
+    "url": "/SanxeReactjs/static/media/sirius-6.2c2a8b11.jpg"
+  },
+  {
+    "revision": "70fe942a601bbc66bb37ab07ebc5191e",
+    "url": "/SanxeReactjs/static/media/sirius-7.70fe942a.jpg"
+  },
+  {
+    "revision": "94553ef4a40e5fbdce288d4feabb8418",
+    "url": "/SanxeReactjs/static/media/sirius-8.94553ef4.jpg"
+  },
+  {
+    "revision": "606f3152fd70e0d1af4e651a1602b6eb",
+    "url": "/SanxeReactjs/static/media/r15-1.606f3152.jpg"
+  },
+  {
+    "revision": "888f5e598899241c510eaed4994004c2",
+    "url": "/SanxeReactjs/static/media/r15-2.888f5e59.jpg"
+  },
+  {
+    "revision": "a94afc6614f56e406dbfa9ce9702a525",
+    "url": "/SanxeReactjs/static/media/r15-3.a94afc66.jpg"
+  },
+  {
+    "revision": "d0744605af6f69ce11e408fae847ce51",
+    "url": "/SanxeReactjs/static/media/r15-4.d0744605.jpg"
+  },
+  {
+    "revision": "0d2bf199d42edbf0b0f94de8199168b9",
+    "url": "/SanxeReactjs/static/media/r15-5.0d2bf199.jpg"
+  },
+  {
+    "revision": "ee0c4f50da4f112188a06ecef8a5acad",
+    "url": "/SanxeReactjs/static/media/r15-6.ee0c4f50.jpg"
+  },
+  {
+    "revision": "74a118e81a4788b485ad7578036d54ce",
+    "url": "/SanxeReactjs/static/media/r15-7.74a118e8.jpg"
+  },
+  {
+    "revision": "583265fb2b7432d1daf4bcb88e18bbaa",
+    "url": "/SanxeReactjs/static/media/r15-8.583265fb.jpg"
+  },
+  {
+    "revision": "197e008ca169595c238b2d8d3dcc4bfe",
+    "url": "/SanxeReactjs/static/media/CBR-1.197e008c.jpg"
+  },
+  {
+    "revision": "ea68326d31e320a3631f461332636279",
+    "url": "/SanxeReactjs/static/media/CBR-2.ea68326d.jpg"
+  },
+  {
+    "revision": "290825bee2dca28ea7b7988d0b57404f",
+    "url": "/SanxeReactjs/static/media/CBR-3.290825be.jpg"
+  },
+  {
+    "revision": "a28e8fec9e0490f13d525fd5ab89e3d8",
+    "url": "/SanxeReactjs/static/media/CBR-4.a28e8fec.jpg"
+  },
+  {
+    "revision": "a28e8fec9e0490f13d525fd5ab89e3d8",
+    "url": "/SanxeReactjs/static/media/CBR-5jpg.a28e8fec.png"
+  },
+  {
+    "revision": "e3a6d244b5351b5517f7392db2940738",
+    "url": "/SanxeReactjs/static/media/CBR-6.e3a6d244.jpg"
+  },
+  {
+    "revision": "ea650500eb3b75fd4096bde4d06b098b",
+    "url": "/SanxeReactjs/static/media/CBR-7.ea650500.jpg"
+  },
+  {
+    "revision": "ae36e32c5bbebd04870a2038a8607481",
+    "url": "/SanxeReactjs/static/media/CBR-8.ae36e32c.jpg"
+  },
+  {
+    "revision": "9e441f871a5d4b5db1f4b97e17cfb128",
+    "url": "/SanxeReactjs/static/media/logo2.9e441f87.png"
+  },
+  {
+    "revision": "2b25f2c57053073176c68722f99864d4",
+    "url": "/SanxeReactjs/static/media/Khuyen Mai.2b25f2c5.webp"
+  },
+  {
+    "revision": "a1e4e54d3215bd928eeea38db76f97dc",
+    "url": "/SanxeReactjs/static/media/img-new-3-1.a1e4e54d.webp"
+  },
+  {
+    "revision": "7ea55a287cffde9d198b7fbedea67c70",
+    "url": "/SanxeReactjs/static/media/img-new-3-2.7ea55a28.webp"
+  },
+  {
+    "revision": "acfadea0b0111bd889de746444c38a14",
+    "url": "/SanxeReactjs/static/media/img-new-3-3.acfadea0.webp"
+  },
+  {
+    "revision": "d621be2f2b1ba85e266c0840f17bb583",
+    "url": "/SanxeReactjs/static/media/img-new-3-4.d621be2f.webp"
+  },
+  {
+    "revision": "3f06feeca08eae0bcb66446c9d59ce86",
+    "url": "/SanxeReactjs/static/media/img-new-3-5.3f06feec.webp"
+  },
+  {
+    "revision": "b8f99345e5e74990bf4784c792cf99b8",
+    "url": "/SanxeReactjs/static/media/img-service.b8f99345.jpg"
+  },
+  {
+    "revision": "4312c401a94440167529246b307d0623",
+    "url": "/SanxeReactjs/static/media/backgroung-img.4312c401.webp"
+  },
+  {
+    "revision": "daa783df47e6845bb6990acdd51fb088",
+    "url": "/SanxeReactjs/index.html"
+  }
+];
